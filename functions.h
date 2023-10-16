22f9c633d68d729a79d6acee255bdb70d9f07894
PaulPark2022 <111074148+PaulPark2022@users.noreply.github.com>
2023-10-15 23:13:34 -0600

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,22 +1,184 @@
 #ifndef FUNCTIONS_H_
 #define FUNCTIONS_H_
 
-#include <algorithm>
 #include <iostream>
-#include <vector>
+#include <string>
+#include <sstream>
 using namespace std;
+
+class Node {
+private:
+    std::string data;
+    Node* prev;
+    Node* next;
+    friend class Functions;
+public:    
+    Node(const std::string&);
+    std::string getData() const;
+};
+
+Node::Node(const std::string& value) : data(value), prev(0), next(0) {}
+
+std::string Node::getData() const {
+    return data;
+}
 
 class Functions {
 public:
-	std::vector<string> sortBooks(std::vector<string> &);
+	Functions();
+	void push(const std::string&);
+	std::string printList();
+	void split(Node*, Node**, Node**);
+	Node* merge(Node*, Node*);
+	void mergeSort();
+	Node* search(const std::string&);
+	bool deleteNode(const std::string&);
+private:
+    Node* head;
+    Node* tail;
 };
 
-std::vector<string> Functions::sortBooks(std::vector<string> &bookList){
-    sort(bookList.begin(), bookList.end());
-   return bookList;
-};
+Functions::Functions() : head(0), tail(0) {}
 
-// Posible idea para usar BST: Almacenar los números de serie de los libros para definir cuáles serán transferidos
+void Functions::push(const std::string& value) {
+        Node* newNode = new Node(value);
+        if (head == 0) {
+            head = tail = newNode;
+        } else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+}
+
+std::string Functions::printList() {
+        std::stringstream aux;   
+        if (head == 0) {
+            return "NULL";
+        }
+        Node* current = head;
+        while (current != 0) {
+            aux << current->data;
+            if (current->next != 0) {
+                aux << ", ";
+            }
+            current = current->next;
+        }
+        return aux.str();
+    }
+
+std::string Functions::reversePrintList() {
+        std::stringstream aux;   
+        if (tail == 0) {
+            return "NULL";
+        }
+        Node* current = tail;
+        while (current != 0) {
+            aux << current->data;
+            if (current->prev != 0) {
+                aux << ", ";
+            }
+            current = current->prev;
+        }
+        return aux.str();
+    }
+
+void Functions::split(Node* source, Node** left, Node** right) {
+        Node* slow = source;
+        Node* fast = source->next;
+
+        while (fast != 0) {
+            fast = fast->next;
+            if (fast != 0) {
+                slow = slow->next;
+                fast = fast->next;
+            }
+        }
+
+        *left = source;
+        *right = slow->next;
+        slow->next = 0;
+        (*right)->prev = 0;
+    }
+
+Node* Functions::merge(Node* left, Node* right) {
+        if (left == nullptr) {
+            return right;
+        }
+        if (right == nullptr) {
+            return left;
+        }
+        
+         Node* result = nullptr;
+        if (left->data <= right->data) {
+            result = left;
+            result->next = merge(left->next, right);
+            result->next->prev = result;
+        } else {
+            result = right;
+            result->next = merge(left, right->next);
+            result->next->prev = result;
+        }
+        return result;
+    }
+
+void Functions::mergeSort() {
+        if (head == nullptr || head->next == nullptr) {
+            return;
+        }
+
+        Node* left;
+        Node* right;
+        split(head, &left, &right);
+        
+        Functions leftList;
+        Functions rightList;
+        leftList.head = left;
+        leftList.tail = left;
+        rightList.head = right;
+        rightList.tail = right;
+
+        leftList.mergeSort();
+        rightList.mergeSort();
+
+        head = merge(leftList.head, rightList.head);
+        tail = rightList.tail;
+
+}
+
+Node* Functions::search(const std::string& value) {
+    
+        Node* current = head;
+        while (current != 0) {
+            if (current->data == value) {
+                return current;
+            }
+            current = current->next;
+        }
+        return 0; 
+    }
+    
+bool Functions::deleteNode(const std::string& value) {
+        Node* current = head;
+        while (current != nullptr) {
+            if (current->data == value) {
+                if (current->prev) {
+                    current->prev->next = current->next;
+                } else {
+                    head = current->next;
+                }
+                if (current->next) {
+                    current->next->prev = current->prev;
+                } else {
+                    tail = current->prev;
+                }
+                
+                delete current;
+                return true;
+            }
+            current = current->next;
+        }
+        return false; 
+    }
 
 #endif /* FUNCTIONS_H_ */
-
